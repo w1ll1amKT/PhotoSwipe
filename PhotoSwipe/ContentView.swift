58aa986a9479d489122955c15fd797745d67c12b
William Kaergaard Thomassen <w1ll1amKTmc@gmail.com>
2026-04-22 13:09:47 +0200

    Initial Commit

---
+++ PhotoSwipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoSwipe
+//
+//  Created by user290288 on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoSwipe/PhotoSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwipeApp.swift
+//  PhotoSwipe
+//
+//  Created by user290288 on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
